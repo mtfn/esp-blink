2ebca2dcbc4531f8ffe025e98817adf24f7f3885
forkpoweroutlet <48189055+yeetus-skeetus@users.noreply.github.com>
2022-10-25 21:38:45 -0700

    Explain myself

--- WiFiAccessPoint/WiFiAccessPoint.ino
+++ WiFiAccessPoint/WiFiAccessPoint.ino
@@ -1,30 +1,29 @@
-/* Create a WiFi access point and provide a web server on it. */
-
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
+/* Some JS trickery that binds buttons to a fetch request */
 const String homepage1 = "<!DOCTYPE html><html><head><meta charset=utf8><title>Test</title><style>button{width:100%;font-size:4em;padding-top:0.5em;padding-bottom:0.5em;}</style></head><body>";
 const String homepage2 = "<script>document.querySelectorAll('button').forEach(function(button){button.addEventListener('click',async function(){fetch('http://192.168.4.1/?input='+button.id);});});</script></body></html>";
 
+/* Will be concatenated between homepage1 & homepage2 */
 String buttons = "";
-
 void addButton(int id, String label) {
   buttons += "<button id=\"" + String(id) + "\">" + label + "</button>";
 }
 
 ESP8266WebServer server(80);
 
-/* Just a little test message.  Go to http://192.168.4.1 in a web browser
-   connected to this access point to see it.  
-*/
+/* Handles the aforementioned fetch request */
 void handler() {
   String input = server.arg("input");
   
   if(input == "") {
+    /* Serve landing page */
     server.send(200, "text/html", homepage1 + buttons + homepage2);
     
   } else {
+    /* Write button ID to serial */
     Serial.write(input.toInt());
     server.send(200, "application/json", "{}");
   }
@@ -35,11 +34,11 @@ void setup() {
   delay(1000);
   Serial.begin(9600);
   
-  /* You can remove the password parameter if you want the AP to be open. */
+  /* Connect to Wifi network with this SSID & pass and visit 192.168.4.1 in browser */
   WiFi.softAP("ESP", "password");
 
   /* Add buttons
-   *  Unique single letter ID in single quotes
+   *  Button ID as int
    *  Button label in double quotes
    */
   addButton(1, "On");
